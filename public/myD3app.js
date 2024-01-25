92b20891d3ca571a0f72d150b623b9d6ed254457
Dominik Masár <dadomasar2@gmail.com>
2024-01-25 01:49:41 +0100

    Add better solution

--- public/myD3app.js
+++ public/myD3app.js
@@ -16,19 +16,13 @@ const By = {
 
 //D3.js canvases
 var buttonArea;
-var barChartArea;
+var recordArea;
 var detailArea;
-var tableArea;
 
 //variables for selection
-var selectedGameId;
+var selectedGameId = -1;
 var chosenGameData;
 var prevChosenGameData;
-
-//color scale
-var myColorScale;
-
-
 
 //variables for precomputed values
 var topValue; //top value in all the data
@@ -52,9 +46,6 @@ d3.csv("./public/steam-library1.csv", function(d) {
   .then(function(csvData) {
     //store loaded data in global variable
     data = csvData;
-    
-    // TODO: takto sortovať:
-    // data = d3.sort(data, (d) => d["hours"]);
 
     //load map and initialise the views
     init();
@@ -70,21 +61,11 @@ function init() {
 
   let width = screen.width;
   let height = screen.height;
-
-  //init selections
-  selectedGameId = 23;
-  prevChosenGameData = data.find(game => game.id === selectedGameId);
-  chosenGameData = data.find(game => game.id === selectedGameId);
     
   //d3 canvases for svg elements
   buttonArea = d3.select("#button_div");
-  barChartArea = d3.select("#barchart_div");
+  recordArea = d3.select("#record_div");
   detailArea = d3.select("#detail_div");
-  
-  /* detailArea = d3.select("#detail_div").append("svg")
-    .attr("width", d3.select("#detail_div").node().clientWidth)
-    .attr("height", d3.select("#detail_div").node().clientHeight); */
-
 
   //computation of top value in all the data
   topValue = 0
@@ -130,127 +111,82 @@ function init() {
     //.style("transform-origin", "top left")
     .text("Popularity")
     .on("click", function () { buttonClick("popularity", "PopularityB"); });
+
+  buttonArea.append("input")
+    .attr("type", "file")
+    .attr("class", "button_normal")
+    .attr("id", "InputB")
+    .style("float", "right")
+    .on("change", handleFile);
+
+  function handleFile() {
+    var input = d3.select("#InputB").node();
+    var file = input.files[0];
+
+    if (file) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        // 'e.target.result' contains the content of the file
+        var fileContent = e.target.result;
+
+        // Remove the BOM if present (UTF-8 with BOM)
+        if (fileContent.charCodeAt(0) === 0xFEFF) {
+          fileContent = fileContent.slice(1);
+        }
+          
+        // Parse the CSV content
+        var tempData = d3.csvParse(fileContent);
+        gameId = 0;
+
+        data = tempData.map((obj) =>
+          ({
+            name: obj["game"],
+            hours: +obj["hours"],
+            userscore: +obj["userscore"],
+            popularity: +obj["userscore_count"],
+            release_date: obj["release_date"],
+            id: gameId++,
+          })
+        );
+      };
+
+        // Read the file as text
+        reader.readAsText(file);
+    }
+  }
 }
 
+function processData(d) {
+  return {
+    name: d["game"],
+    hours: +d["hours"],
+    userscore: +d["userscore"],
+    popularity: +d["userscore_count"],
+    release_date: d["release_date"],
+    id: gameId++,
+    //multiplayer: d["multiplayer any"],
+  }
+}
 
 /*----------------------
 BEGINNING OF VISUALIZATION
 ----------------------*/
 function visualization() {
-
-  //drawTextInfo();
-
   drawTable();
-
-  drawDetailArea();
-
-}
-
-
-/*----------------------
-BAR CHART
-----------------------*/
-function drawBarChart(region) {
-
-  //clear all child nodes from barchart SVG canvas (all rects and texts)
-  barChartArea.selectAll("*").remove()
-
-  //get area width/height
-  let thisCanvasHeight = barChartArea.node().clientHeight
-
-  //interate over rows in the data
-  /*
-  for (let index = 0; index < data.length; index++) {
-
-    //compute old bar height with respect to the represented value and availible space
-    var previousBarHeight = (data[index][previousSelectedRegion] / topValue) * thisCanvasHeight
-
-    //compute new bar height with respect to the represented value and availible space
-    var barHeight = (data[index][region] / topValue) * thisCanvasHeight
-
-
-
-    //append a bar to the barchart
-    barChartArea.append('rect') 
-      .attr("x", labelWidth + index * barWidth)//attributes before transition
-      .attr("y", thisCanvasHeight - previousBarHeight)
-      .attr("width", barWidth + 1)
-      .attr("height", previousBarHeight)
-      .attr("fill", "darkblue")
-      .on("click", function () { chartClick(index); }) //registering the click event and folow up action
-      .transition() //transition animation
-        .duration(1000)
-        .attr("y", thisCanvasHeight - barHeight)//attributes after transition
-        .attr("height", barHeight)
-  }
-
-  //intialize year variable
-  var year = ""
-
-  //iterate over rows in the data
-  for (let index = 0; index < data.length; index++) {
-
-    //test for change of the year, if the year changes, append the text label to the barchart
-    if (data[index].date.substr(0, 4) != year) {
-
-      year = data[index].date.substr(0, 4)
-
-      barChartArea.append("text")
-        .attr("x", labelWidth + index * barWidth)
-        .attr("y", thisCanvasHeight)
-        .attr("class", "subline")
-        .style('fill', 'white')
-        .text(year)
-    }
-  }
-
-  //append rectagle outlining the selected timepoint
-  timeIndicatorBarchart = barChartArea.append('rect')
-    .attr("x", labelWidth + selectedIndex*barWidth)
-    .attr("y", 0)
-    .attr("width", barWidth)
-    .attr("height", thisCanvasHeight)
-    .attr("fill", "none")
-    .attr("stroke", "white")
-    .attr("opacity", 0.7)
-    .style("stroke-width", 3)
-
-
-  /*
-  //Square transition example
-  barChartArea.append('rect')
-    .attr("x", thisCanvasWidth / 3) 
-    .attr("y", thisCanvasHeight / 3) 
-    .attr("width", 80) 
-    .attr("height", 80) 
-    .attr("fill", "red" )
-    .transition()
-      .duration(5000)
-      .attr("x", 2 * thisCanvasWidth / 3)
-      .attr("y", 2 * thisCanvasHeight / 3)
-      .attr("width", 40)
-      .attr("height", 40) 
-      .attr("fill", "blue" );
-  */
-
-  
-
 }
 
 /*----------------------
 TABLE
 ----------------------*/
 function drawTable() {
-  barChartArea.selectAll("*").remove();
+  recordArea.selectAll("*").remove();
 
   for (let index = 0; index < data.length; index++) {
-    barChartArea.append("div")
+    recordArea.append("div")
         .style("margin-top", "1%")
         .style("margin-right", "1%")
         .attr("class", "button-54")
-        //.style("transform", `scale(${1.2})`)
-        //.style("transform-origin", "top left")
-        //.style("width", `${100 / 1.29}%`)
         .text(shortenString(data[index].name, maxRowLength))
         .on("click", function () { gameClick(data[index]); });
   }
@@ -294,6 +230,16 @@ function drawDetailArea() {
     .text(`${game.hours}`)
 
   detailArea.append("div")
+    .attr("id", "barchart")
+    .style("margin-left", "1em")
+    .style("margin-top", "1em")
+    .style("height", "20vh")
+    .style("width", "100%")
+    .style("float", "left");
+
+  gameHours(barchart);
+
+  detailArea.append("div")
     .attr("class", "detail__label")
     .style("margin-left", "1.5em")
     .style("margin-top", "1em")
@@ -305,7 +251,7 @@ function drawDetailArea() {
     .style("font-size", "15px")
     .text(`${game.popularity}`);
 
-    detailArea.append("div")
+  detailArea.append("div")
     .attr("class", "detail__label")
     .style("margin-left", "1.5em")
     .style("margin-top", "1em")
@@ -316,23 +262,15 @@ function drawDetailArea() {
     .style("margin-left", "2em")
     .style("font-size", "15px")
     .text(`${game.release_date}`);
-
-  detailArea.append("div")
-    .attr("id", "barchart")
-    .style("margin-left", "1em")
-    .style("margin-top", "1em")
-    .style("height", "20vh")
-    .style("width", "100%")
-    .style("float", "left");
-
-    gameHours(barchart);
 }
 
 function gameHours(barchart) {
   // Sort the data by hours played
   var sortedData = data.slice().sort((a, b) => b.hours - a.hours);
+  
+  sortedData = sortedData.filter(x => x.hours > 5);
+  //sortedData = sortedData.filter(x => x.hours < 50);
   sortedData.map((x, i) => {x.index = i; return x});
-  sortedData = sortedData.filter(x => x.hours > 20);
   console.log(sortedData);
 
   // Create a linear scale for the x-axis
@@ -397,28 +335,6 @@ function gameHours(barchart) {
     .attr('d', areaGenerator(sortedData))
     .style('fill', 'url(#areaGradient)');
 
-  var xAxis = svg.append('g')
-    .attr('class', 'grid x-grid')
-    .attr('id', 'xGrid');
-
-  xAxis.append('line')
-    .attr('x1', '0')
-    .attr('x2', '0')
-    .attr('y1', '0')
-    .attr('y2', barchart.offsetHeight)
-    .attr('stroke-width', '2')
-    .attr('stroke', 'black');
-
-  var maxHours = d3.max(sortedData, (d) => d.hours);
-
-  for (let i = 0; i < 5; i++) 
-  {
-    svg.append('text')
-      .attr('x', '')
-      .attr('y', '')
-  }
-
-
   // Create the highlighted area
   svg.append('path')
     .attr('class', 'highlight-area')
@@ -440,37 +356,10 @@ function gameHours(barchart) {
   line_chart.attr("d", `${val}L${barchart.offsetWidth},0`)
 }
 
-/*----------------------
-INTERACTION
-----------------------*/
-function mapClick(region) {
-  console.log(region)
-
-  //store previous and new selection
-  previousSelectedRegion = selectedRegion
-  selectedRegion = region;
-
-  //set the text descrition
-  selectedAreaText.text("Selected Region: " + selectedRegion.replace(/_/g, " "));
-
-  //remove highlighted outline from previous region
-  d3.select("#map").select("#" + previousSelectedRegion).style("stroke", "gray");
-
-  //add highlighted outline to new selected region  
-  d3.select("#map").select("#" + selectedRegion).style("stroke", "white");
-
-  //move selected region to the top layer of the svg graphics (to avoid problems with overlapping contours)
-  d3.select("#map").select("#" + selectedRegion).raise();
-
-  //redraw barchart with newly selected region
-  drawBarChart(selectedRegion);
-
-}
-
 function gameClick(game) {
   console.log(game.id)
   selectedGameId = game.id;
-  prevChosenGameData = chosenGameData;
+  prevChosenGameData = (chosenGameData) ? chosenGameData : game;
   chosenGameData = game;
 
   drawDetailArea();
